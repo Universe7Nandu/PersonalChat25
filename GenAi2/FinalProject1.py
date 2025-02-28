64d3863b19b9cd8c5a09979a5582f20c7e281e9d
Nandesh <114307941+Universe7Nandu@users.noreply.github.com>
2025-02-28 21:11:27 +0530

    Update FinalProject1.py

--- GenAi2/FinalProject1.py
+++ GenAi2/FinalProject1.py
@@ -351,7 +351,7 @@ def chatgpt_like_ui():
             for i, ch in enumerate(st.session_state.chat_history):
                 st.write(f"**{i+1}.** {ch['query']}")
         st.markdown("---")
-        st.image('photo2.JPG', use_container_width=True)
+        st.image(os.path.join(os.getcwd(), 'photo2.jpg'), use_container_width=True)
         st.write("**Nandesh Kalashetti**")
         st.write("GenAi Developer And Full-stack Web-Developer")
         st.markdown("[LinkedIn](https://www.linkedin.com/in/nandesh-kalashetti-333a78250/)")
